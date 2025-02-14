540d723d5c311fc08399605a62dd8157dbd55bcd
narikko <116518028+narikko@users.noreply.github.com>
2025-02-14 18:38:58 -0500

    Setting up backend

---
+++ JerseyCabinet-Backend/src/main/java/ca/mcgill/ecse428/jerseycabinet/JerseycabinetApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.jerseycabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseycabinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseycabinetApplication.class, args);
+	}
+
+}
---
+++ JerseyCabinet-Backend/src/test/java/ca/mcgill/ecse428/jerseycabinet/JerseycabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.jerseycabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerseycabinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
