43a1eb4830dbadd812d5d150ad16944e52eb99e4
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2025-02-23 19:42:52 -0500

    all cucumber tests pass

--- jersey-cabinet-backend/src/test/java/ca/mcgill/ecse428/jerseycabinet/acceptance/AuthenticationStepDefinitions.java
+++ jersey-cabinet-backend/src/test/java/ca/mcgill/ecse428/jerseycabinet/acceptance/AuthenticationStepDefinitions.java
@@ -2,7 +2,7 @@ package ca.mcgill.ecse428.jerseycabinet.acceptance;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.ArrayList;
 
@@ -36,41 +36,36 @@ public class AuthenticationStepDefinitions {
     private AuthenticationService authenService;
 
     private Jersey jersey1;
-    private Jersey jersey2;
-    private Jersey jersey3;
+    private int id;
+
 
     private Employee employee;
     private Customer customer;
     ArrayList<Jersey> unlisted_jerseys;
-
+ 
+    
     @After
     public void cleanup(){
-        jerseyRepo.delete(jersey1);
-        jerseyRepo.delete(jersey2);
-        jerseyRepo.delete(jersey3);
-        employeeRepo.delete(employee);
-        customerRepo.delete(customer);
+        jerseyRepo.delete(this.jersey1);
+        employeeRepo.delete(this.employee);
+        customerRepo.delete(this.customer);
     }
 
   
-    @Given("the following authentication requests exist in the system")
+    @Given("the following authentication request exists in the system")
     public void the_following_authentication_requests_exist_in_the_system(io.cucumber.datatable.DataTable dataTable){
         customer = new Customer("tee@gmail.com","hehe123","123 Main Street");
 
-        jersey1 = new Jersey(null,"This is a jersey.","Nike","soccer","black",null,null,null,customer);
-        jersey2 = new Jersey(null,"A jersey this is.","Adidas","basketball","blue",null,null,null,customer);
-        jersey3 = new Jersey(null,"Is this a jersey?","Puma","football","orange",null,null,null,customer);
-
-        customerRepo.save(customer);
-        jerseyRepo.save(jersey1);
-        jerseyRepo.save(jersey2);
-        jerseyRepo.save(jersey3);
+        this.jersey1 = new Jersey(null,"This is a jersey.","Nike","soccer","black",null,null,customer);
+        customerRepo.save(this.customer);
+        jerseyRepo.save(this.jersey1);
+        this.id = this.jersey1.getId();
     }
 
-    @And("I am logged in as an employee")
+    @And("I am logged in as an Employee")
     public void i_am_logged_in_as_an_employee(){
-        employee = new Employee("a@jerseycabinet.com","jersey123");
-        employeeRepo.save(employee);
+        this.employee = new Employee("a@jerseycabinet.com","jersey123");
+        employeeRepo.save(this.employee);
     }
 
     @And("I am viewing the list of pending requests")
@@ -78,53 +73,53 @@ public class AuthenticationStepDefinitions {
         this.unlisted_jerseys = authenService.findAllUnreviewedJerseys();
     }
 
-    @When("I confirm the authencity of the jersey {int}")
-    public void i_confirm_the_authencity_of_the_jersey(int id) throws Exception{
-        authenService.findJerseyById(id);
-        authenService.acceptRequestById(id,employee);
+    @When("I confirm the authencity of the jersey")
+    public void i_confirm_the_authencity_of_the_jersey() throws Exception{
+        authenService.findJerseyById(this.id);
+        authenService.acceptRequestById(this.id,this.employee);
     }
 
-    @Then("the jersey {int} is marked as unlisted")
-    public void the_jersey_is_marked_as_unlisted(int id) throws Exception{
-        Jersey jersey = authenService.findJerseyById(id);
+    @Then("the jersey is marked as unlisted")
+    public void the_jersey_is_marked_as_unlisted() throws Exception{
+        Jersey jersey = authenService.findJerseyById(this.id);
         assertEquals(RequestState.Unlisted,jersey.getRequestState());
     }
 
-    @And("the jersey {int} is assigned to me.")
-    public void the_jersey_is_assigned_to_the_employee_who_made_the_decision(int id) throws Exception{
-        Jersey jersey = authenService.findJerseyById(id);
-        assertEquals(employee,jersey.getEmployee());
+    @And("the jersey is assigned to me.")
+    public void the_jersey_is_assigned_to_the_employee_who_made_the_decision() throws Exception{
+        Jersey jersey = authenService.findJerseyById(this.id);
+        assertEquals(employee.getId(),jersey.getEmployee().getId());
     }
 
-    @And("I reject the authencity of the jersey {int}")
-        public void i_reject_the_authencity_of_the_jersey(int id) throws Exception{
-            authenService.findJerseyById(id);
-            authenService.rejectRequestById(id,employee);
+    @And("I reject the authencity of the jersey")
+        public void i_reject_the_authencity_of_the_jersey() throws Exception{
+            authenService.findJerseyById(this.id);
+            authenService.rejectRequestById(this.id,this.employee);
         }
     
 
-    @Then("the jersey {int} is marked as rejected")
-    public void the_jersey_is_marked_as_rejected(int id) throws Exception{
-        Jersey jersey = authenService.findJerseyById(id);
+    @Then("the jersey is marked as rejected")
+    public void the_jersey_is_marked_as_rejected() throws Exception{
+        Jersey jersey = authenService.findJerseyById(this.id);
         assertEquals(RequestState.Rejected,jersey.getRequestState());
     }
 
 
-    @When("I view a request {int} but do not make a decision ")
-    public void i_request_to_view_but_do_not_make_a_decision(int id) throws Exception{
-        authenService.findJerseyById(id);
+    @When("I view a request but do not make a decision")
+    public void i_request_to_view_but_do_not_make_a_decision() throws Exception{
+        authenService.findJerseyById(this.id);
     }
 
-    @Then("the jersey {int} status remains unchanged")
-    public void the_jersey_status_remains_unchanged(int id) throws Exception{
-        Jersey jersey = authenService.findJerseyById(id);
+    @Then("the jersey status remains unchanged")
+    public void the_jersey_status_remains_unchanged() throws Exception{
+        Jersey jersey = authenService.findJerseyById(this.id);
         assertNull(jersey.getRequestState());
     }
     
-    @And("the jersey {int} remains in the list of requests to be reviewed")
-    public void the_jersey_remains_in_the_list_of_requests_to_be_reviewed(int id){
-
-
+    @And("the jersey remains in the list of requests to be reviewed.")
+    public void the_jersey_remains_in_the_list_of_requests_to_be_reviewed(){
+        assertNotNull(authenService.findAllUnreviewedJerseys());
+        assertEquals(authenService.findAllUnreviewedJerseys().get(1).getId(),jersey1.getId());
     }
     
    
--- jersey-cabinet-backend/src/test/resources/US002_review_auth_requests.feature
+++ jersey-cabinet-backend/src/test/resources/US002_review_auth_requests.feature
@@ -5,26 +5,24 @@ I want to review authentication requests
 so that I can confirm or descredit the authenticity of a jersey.
 
 Background:
-    Given the following authentication requests exist in the system
-        | id |requestState| description       | brand | sport      | color | jerseyImage | proofOfAuthenticationImage | Employee | Customer
-        | 2  | null       | This is a jersey. | Nike  | soccer     | black | null        | null                       | null     | "tee@gmail.com"
-        | 3  | null       | A jersey this is. | Adidas| basketball | blue  | null        | null                       | null     | "tee@gmail.com"
-        | 4  | null       | Is this a jersey? | Puma  | football   | orange| null        | null                       | null     | "tee@gmail.com"
-
+    Given the following authentication request exists in the system
+        | requestState| description       | brand | sport      | color | jerseyImage | proofOfAuthenticationImage | Employee | Customer
+        | null       | This is a jersey. | Nike  | soccer     | black | null        | null                       | null     | "tee@gmail.com"
     And I am logged in as an Employee
     And I am viewing the list of pending requests
 
 Scenario Outline: Confirm the authenticity request. (Normal flow)
-    When I confirm the authencity of the jersey "<id>"
-    And the jersey "<id>" is marked as unlisted
-    And the jersey "<id>" is assigned to me. 
+    When I confirm the authencity of the jersey 
+    And the jersey is marked as unlisted
+    And the jersey is assigned to me. 
+
 
 Scenario Outline: Reject the authenticity request. (Normal flow)
-    When I reject the authencity of the jersey "<id>" 
-    And the jersey "<id>" is marked as rejected
-    And the jersey "<id>" is assigned to me. 
+    When I reject the authencity of the jersey 
+    And the jersey is marked as rejected
+    And the jersey is assigned to me. 
 
 Scenario Outline: I do not make a decision. (Error flow)
-    When I view a request "<id>" but do not make a decision 
-    Then the jersey "<id>" status remains unchanged
-    And the jersey "<id>" remains in the list of requests to be reviewed.
+    When I view a request but do not make a decision 
+    Then the jersey status remains unchanged
+    And the jersey remains in the list of requests to be reviewed.
