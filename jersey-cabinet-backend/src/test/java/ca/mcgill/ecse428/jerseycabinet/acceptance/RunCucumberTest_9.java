2fea05ababd2f11ac4bfbd277f994d8f819b1ddc
narikko <116518028+narikko@users.noreply.github.com>
2025-02-25 16:16:57 -0500

    US009 Refactor (tests passing)

--- jersey-cabinet-backend/src/test/java/ca/mcgill/ecse428/jerseycabinet/acceptance/RunCucumberTest_9.java
+++ jersey-cabinet-backend/src/test/java/ca/mcgill/ecse428/jerseycabinet/acceptance/RunCucumberTest_9.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/US009_authentification_form.feature", glue = "ca.mcgill.ecse428.jerseycabinet.acceptance")
-public class RunCucumberTestAuthentification {
+@CucumberOptions(features = "src/test/resources/US009_authentification_form.feature", glue = "ca.mcgill.ecse428.jerseycabinet.acceptance.US009")
+public class RunCucumberTest_9 {
 
 }
