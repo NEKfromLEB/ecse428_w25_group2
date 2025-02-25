a0c06c2f610fcddc285533ae24ceba1370c5e980
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2025-02-25 17:56:55 -0500

    Add Cucumber step definitions for US001

---
+++ jersey-cabinet-backend/src/test/java/ca/mcgill/ecse428/jerseycabinet/acceptance/RunCucumberSteps.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse428.jerseycabinet.acceptance;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/US001_jersey_offer_submission.feature", glue = "ca.mcgill.ecse428.jerseycabinet.acceptance")
+public class RunCucumberSteps {
+}
