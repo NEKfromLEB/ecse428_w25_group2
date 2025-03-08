8e2c1f0083a7d4dd661b9b8c33935094bddfd2ab
narikko <116518028+narikko@users.noreply.github.com>
2025-03-07 20:31:05 -0500

    Service methods for US013 and added a few error checks for place offer

--- jersey-cabinet-backend/src/main/java/ca/mcgill/ecse428/jerseycabinet/service/OfferService.java
+++ jersey-cabinet-backend/src/main/java/ca/mcgill/ecse428/jerseycabinet/service/OfferService.java
@@ -1,13 +1,16 @@
 package ca.mcgill.ecse428.jerseycabinet.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import ca.mcgill.ecse428.jerseycabinet.dao.EmployeeRepository;
 import ca.mcgill.ecse428.jerseycabinet.dao.JerseyRepository;
 import ca.mcgill.ecse428.jerseycabinet.dao.OfferRepository;
 import ca.mcgill.ecse428.jerseycabinet.model.Employee;
 import ca.mcgill.ecse428.jerseycabinet.model.Jersey;
+import ca.mcgill.ecse428.jerseycabinet.model.Jersey.RequestState;
 import ca.mcgill.ecse428.jerseycabinet.model.Offer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import ca.mcgill.ecse428.jerseycabinet.model.Offer.OfferState;
 
 @Service
 public class OfferService {
@@ -35,12 +38,59 @@ public class OfferService {
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new IllegalArgumentException("Employee not found"));
 
+        if (jersey.getRequestState() != RequestState.Unlisted){
+            throw new IllegalArgumentException("Invalid jersey state.");
+        }
+
         // 4) Create the Offer
         Offer.OfferKey key = new Offer.OfferKey(jersey, employee);
         Offer offer = new Offer(key, Offer.OfferState.Pending, price);
 
+        if (offerRepository.findOfferByKey(key) != null){
+            throw new IllegalArgumentException("You have already placed an offer for this jersey.");
+        }
+
         // 5) Save and return
         return offerRepository.save(offer);
+    }
+
+    public Offer acceptOffer(int jerseyId, int employeeId){
+        Jersey jersey = jerseyRepository.findById(jerseyId)
+                .orElseThrow(() -> new IllegalArgumentException("Jersey not found"));
+
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new IllegalArgumentException("Employee not found"));
+
+        if (jersey.getRequestState() != RequestState.Unlisted){
+            throw new IllegalArgumentException("Invalid jersey state.");
+        }
+
+        Offer.OfferKey key = new Offer.OfferKey(jersey, employee);
+        Offer offer = offerRepository.findOfferByKey(key);
+
+        offer.setOfferState(OfferState.Accepted);
+        jersey.setRequestState(RequestState.Listed);
+
+        return offerRepository.save(offer);
+    }
+
+    public Offer rejectOffer(int jerseyId, int employeeId){
+        Jersey jersey = jerseyRepository.findById(jerseyId)
+                .orElseThrow(() -> new IllegalArgumentException("Jersey not found"));
+
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new IllegalArgumentException("Employee not found"));
+
+        if (jersey.getRequestState() != RequestState.Unlisted){
+            throw new IllegalArgumentException("Invalid jersey state.");
+        }
+        
+        Offer.OfferKey key = new Offer.OfferKey(jersey, employee);
+        Offer offer = offerRepository.findOfferByKey(key);
+
+        offer.setOfferState(OfferState.Rejected);
+
+        return offer;
     }
 }
 
