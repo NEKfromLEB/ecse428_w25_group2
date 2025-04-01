9035cf1446d0c570ae41db17401451a26d6814c1
narikko <116518028+narikko@users.noreply.github.com>
2025-04-01 19:23:46 -0400

    us013 done

--- jersey-cabinet-backend/src/main/java/ca/mcgill/ecse428/jerseycabinet/service/OfferService.java
+++ jersey-cabinet-backend/src/main/java/ca/mcgill/ecse428/jerseycabinet/service/OfferService.java
@@ -11,6 +11,7 @@ import ca.mcgill.ecse428.jerseycabinet.model.Jersey;
 import ca.mcgill.ecse428.jerseycabinet.model.Jersey.RequestState;
 import ca.mcgill.ecse428.jerseycabinet.model.Offer;
 import ca.mcgill.ecse428.jerseycabinet.model.Offer.OfferState;
+import jakarta.transaction.Transactional;
 
 @Service
 public class OfferService {
@@ -24,6 +25,10 @@ public class OfferService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private ListJerseyService listJerseyService;
+
+    @Transactional
     public Offer placeOffer(int jerseyId, int employeeId, int price) {
         // 1) Check for negative price
         if (price < 0) {
@@ -54,6 +59,7 @@ public class OfferService {
         return offerRepository.save(offer);
     }
 
+    @Transactional
     public Offer acceptOffer(int jerseyId, int employeeId){
         Jersey jersey = jerseyRepository.findById(jerseyId)
                 .orElseThrow(() -> new IllegalArgumentException("Jersey not found"));
@@ -62,18 +68,24 @@ public class OfferService {
                 .orElseThrow(() -> new IllegalArgumentException("Employee not found"));
 
         if (jersey.getRequestState() != RequestState.Unlisted){
-            throw new IllegalArgumentException("Invalid jersey state.");
+            throw new IllegalArgumentException("An offer has been previously accepted for this jersey.");
         }
 
         Offer.OfferKey key = new Offer.OfferKey(jersey, employee);
         Offer offer = offerRepository.findOfferByKey(key);
 
-        offer.setOfferState(OfferState.Accepted);
-        jersey.setRequestState(RequestState.Listed);
+        if (offer == null){
+            throw new IllegalArgumentException("cant find offer");
+        }
 
-        return offerRepository.save(offer);
+        offer.setOfferState(OfferState.Accepted);
+        listJerseyService.listJerseyForSale(jerseyId);
+
+        offer = offerRepository.save(offer);
+        return offer;
     }
 
+    @Transactional
     public Offer rejectOffer(int jerseyId, int employeeId){
         Jersey jersey = jerseyRepository.findById(jerseyId)
                 .orElseThrow(() -> new IllegalArgumentException("Jersey not found"));
@@ -82,18 +94,24 @@ public class OfferService {
                 .orElseThrow(() -> new IllegalArgumentException("Employee not found"));
 
         if (jersey.getRequestState() != RequestState.Unlisted){
-            throw new IllegalArgumentException("Invalid jersey state.");
+            throw new IllegalArgumentException("An offer has been previously accepted for this jersey.");
         }
         
         Offer.OfferKey key = new Offer.OfferKey(jersey, employee);
         Offer offer = offerRepository.findOfferByKey(key);
 
         offer.setOfferState(OfferState.Rejected);
-
+        
+        offer = offerRepository.save(offer);
         return offer;
     }
 
+    @Transactional
     public Offer counterOffer(int jerseyId, int employeeId, int price){
+        if (price < 0) {
+            throw new IllegalArgumentException("Please enter a non-negative offer amount.");
+        }
+
         Jersey jersey = jerseyRepository.findById(jerseyId)
                 .orElseThrow(() -> new IllegalArgumentException("Jersey not found"));
 
@@ -103,10 +121,13 @@ public class OfferService {
         Offer.OfferKey key = new Offer.OfferKey(jersey, employee);
         Offer offer = offerRepository.findOfferByKey(key);
 
+        if (offer.getOfferState() == OfferState.Accepted){
+            throw new IllegalArgumentException("Accepted offers cannot be modified.");
+        }
+
         offer.setOfferState(OfferState.Pending);
         offer.setPrice(price);
 
         return offerRepository.save(offer);
     }
 }
-
