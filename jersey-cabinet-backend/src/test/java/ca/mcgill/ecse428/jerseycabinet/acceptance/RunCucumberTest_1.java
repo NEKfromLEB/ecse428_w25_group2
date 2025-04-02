b0fb95558a5ef7c70cd0a6861c61f43538b18358
narikko <116518028+narikko@users.noreply.github.com>
2025-04-01 23:23:11 -0400

    Update RunCucumberTest_1.java

--- jersey-cabinet-backend/src/test/java/ca/mcgill/ecse428/jerseycabinet/acceptance/RunCucumberTest_1.java
+++ jersey-cabinet-backend/src/test/java/ca/mcgill/ecse428/jerseycabinet/acceptance/RunCucumberTest_1.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/US_21_apply_discount_to_category.feature", glue = "ca.mcgill.ecse428.jerseycabinet.acceptance.US0021")
+@CucumberOptions(features = "src/test/resources/US001_jersey_offer_submission.feature", glue = "ca.mcgill.ecse428.jerseycabinet.acceptance.US001")
 public class RunCucumberTest_1 {
 }
